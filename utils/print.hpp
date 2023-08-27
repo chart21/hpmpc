320d608700f2dd85d4e7cf904b45fd498089c593
chart21 <chart21@web.de>
2023-08-27 17:05:56 +0200

    temporary disabled party identifiers during print

--- utils/print.hpp
+++ utils/print.hpp
@@ -6,7 +6,7 @@ void print(const char* format, ...) {
     va_list args;
     va_start(args, format);
 
-    printf("P%i: ", PARTY);
+    /* printf("P%i: ", PARTY); */
     vprintf(format, args);
 
     va_end(args);
