1074be797613baf57b1935205413cff63182d6ff
chart21 <chart21@web.de>
2023-12-10 03:11:57 +0100

    AVX512 fix

--- arch/AVX512.h
+++ arch/AVX512.h
@@ -178,17 +178,17 @@ inline __m512i _mm512_slli_epi64_wrapper(__m512i a) {
 // shift right logical
 template <int n>
 inline __m512i __mm512_srl_epi16_wrapper(__m512i a) {
-  return _mm512_srl_epi16(a,n);
+  return _mm512_srli_epi16(a,n);
 }
 
 template <int n>
 inline __m512i __mm512_srl_epi32_wrapper(__m512i a) {
-  return _mm512_srl_epi32(a,n);
+  return _mm512_srli_epi32(a,n);
 }
 
 template <int n>
 inline __m512i __mm512_srl_epi64_wrapper(__m512i a) {
-  return _mm512_srl_epi64(a,n);
+  return _mm512_srli_epi64(a,n);
 }
 
 
