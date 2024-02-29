61cb026a453a6b520e22c54ecb56ad000a5923d9
Christopher Knop <chris.d.k.knop@gmail.com>
2024-02-29 16:43:03 +0100

    input from multiple files

--- MP-SPDZ/lib/help/Input.hpp
+++ MP-SPDZ/lib/help/Input.hpp
@@ -20,8 +20,8 @@ namespace IR {
 
 class Input {
   public:
-    explicit Input(const std::string& path, const int& player_num)
-        : player_num(player_num), cur(0) {
+    explicit Input(const std::string& path, const int& player_num, const int& cur_vec)
+        : player_num(player_num), vec(cur_vec), cur(0) {
         int fd = open(path.c_str(), O_RDONLY);
         if (fd < 0) {
             log(Level::ERROR, "couldn 't open file \"", path, "\"");
@@ -73,9 +73,11 @@ class Input {
     T next(std::function<T(const std::string&)> convert);
 
     inline int get_player() const { return player_num; };
+    inline int get_vec_num() const { return vec; };
 
   private:
     int player_num;
+    unsigned vec;
     char* file;
     size_t size;
     size_t cur;
@@ -98,44 +100,48 @@ T Input::next(std::function<T(const std::string&)> convert) {
 
 static std::vector<Input> inputs;
 
-std::vector<Input>::iterator get_input_from(const int& player_num) {
+std::vector<Input>::iterator get_input_from(const int& player_num, const size_t& cur_vec) {
     auto it = inputs.begin();
     for (; it != inputs.end(); ++it) {
-        if (it->get_player() == player_num)
+        if (it->get_player() == player_num && it->get_vec_num() == cur_vec)
             return it;
     }
 
     return it;
 }
 
-int next_input(const int& player_num, const int& thread_id) {
+std::array<int, SIZE_VEC> next_input(const int& player_num, const int& thread_id) {
+    std::array<int, SIZE_VEC> res;
     if (current_phase == PHASE_INIT || player_num != PARTY)
-        return 0;
+        return res;
 
-    auto in = get_input_from(player_num);
-    if (in == inputs.end()) {
-        inputs.emplace_back(ROOT_DIR + "/Input-P" + std::to_string(player_num) + "-" +
-                                std::to_string(thread_id),
-                            player_num);
-        in = inputs.end() - 1;
+    for (size_t cur = 0; cur < SIZE_VEC; cur++) {
+        auto in = get_input_from(player_num, cur);
+        if (in == inputs.end()) {
+            inputs.emplace_back(ROOT_DIR + "/Input-P" + std::to_string(player_num) + "-" +
+                                    std::to_string(thread_id) + "-" + std::to_string(cur),
+                                player_num, cur);
+            in = inputs.end() - 1;
+        }
+
+        int a = in->template next<int>(
+            [](const std::string& s) -> int { return std::stoi(s.c_str(), nullptr, 10); });
+
+        print("READ: %i\n", a);
+        res[cur] = a;
     }
-
-    int a = in->template next<int>(
-        [](const std::string& s) -> int { return std::stoi(s.c_str(), nullptr, 10); });
-
-    print("READ: %i\n", a);
-    return a;
+    return res;
 }
 
 float next_input_f(const int& player_num, const int& thread_id) {
     if (player_num != PARTY)
         return 1;
 
-    auto in = get_input_from(player_num);
+    auto in = get_input_from(player_num, 0);
     if (in == inputs.end()) {
         inputs.emplace_back(ROOT_DIR + "/Input-P" + std::to_string(player_num) + "-" +
-                                std::to_string(thread_id),
-                            player_num);
+                                std::to_string(thread_id) + "-0",
+                            player_num, 0);
         in = inputs.end() - 1;
     }
 
