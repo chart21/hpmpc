d5ad89e54623e5586a7552491d3891e8ea4f5a10
Christopher Knop <christopher.knop@tum.de>
2024-03-03 01:09:14 +0100

    sse: for next_float and next_bit

--- MP-SPDZ/lib/help/Input.hpp
+++ MP-SPDZ/lib/help/Input.hpp
@@ -133,30 +133,33 @@ std::array<int, SIZE_VEC> next_input(const int& player_num, const int& thread_id
     return res;
 }
 
-float next_input_f(const int& player_num, const int& thread_id) {
-    if (player_num != PARTY)
-        return 1;
+std::array<float, SIZE_VEC> next_input_f(const int& player_num, const int& thread_id) {
+    std::array<float, SIZE_VEC> res;
+    if (current_phase == PHASE_INIT || player_num != PARTY)
+        return res;
 
-    auto in = get_input_from(player_num, 0);
-    if (in == inputs.end()) {
-        inputs.emplace_back(ROOT_DIR + "/Input-P" + std::to_string(player_num) + "-" +
-                                std::to_string(thread_id) + "-0",
-                            player_num, 0);
-        in = inputs.end() - 1;
+    for (size_t cur = 0; cur < SIZE_VEC; ++cur) {
+        auto in = get_input_from(player_num, 0);
+        if (in == inputs.end()) {
+            inputs.emplace_back(ROOT_DIR + "/Input-P" + std::to_string(player_num) + "-" +
+                                    std::to_string(thread_id) + "-" + std::to_string(cur),
+                                player_num, 0);
+            in = inputs.end() - 1;
+        }
+
+        float a = in->template next<float>(
+            [](const std::string& s) -> float { return std::stof(s.c_str(), nullptr); });
+        res[cur] = a;
     }
-
-    float a = in->template next<float>(
-        [](const std::string& s) -> float { return std::stof(s.c_str(), nullptr); });
-
-    return a;
+    return res;
 }
 
 uint8_t get_next_bit(const int& player_id) {
-    if (player_id != PARTY)
+    if (current_phase == PHASE_INIT || player_id != PARTY)
         return 0;
 
     if (bit_queue.empty()) {
-        auto input = next_input_f(player_id, 0);
+        auto input = next_input_f(player_id, 0)[0];
         long in = std::round(input);
 
         for (size_t i = 0; i < BITLENGTH; i++) {
