e6eab90c97ba58f231535f6fe36c7d391deef1a5
chart21 <chart21@web.de>
2024-03-29 07:39:06 +0100

    changed default config

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MODELOWNER P_0 //Who holds the model parameters? (-1: Dummy model parameters, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing)
-#define DATAOWNER P_1 //Who holds the data? (-1: Dummy dataset, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing)
-#define PROTOCOL 12
+#define MODELOWNER -1 //Who holds the model parameters? (-1: Dummy model parameters, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing)
+#define DATAOWNER -1 //Who holds the data? (-1: Dummy dataset, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing)
+#define PROTOCOL 5
 
 // Party ID (starting from 0)
 #define PARTY 3
