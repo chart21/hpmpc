86e2f8b2e079b3a28ad05e5e99fb212def298a6a
aSlunk <chris.d.k.knop@gmail.com>
2024-04-04 19:37:15 +0200

    bench scripts

---
+++ MP-SPDZ/bench_scripts/host_setup.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Global setup-script running locally on experiment server. 
+# Initializing the experiment server
+
+# exit on error
+set -e             
+# log every command
+set -x                         
+
+REPO=$(pos_get_variable repo_hpmpc --from-global)
+REPO_COMMIT=$(pos_get_variable repo_hpmpc_commit --from-global)       
+REPO_DIR=$(pos_get_variable repo_hpmpc_dir --from-global)
+REPO2=$(pos_get_variable repo --from-global)
+REPO2_DIR=$(pos_get_variable repo_dir --from-global)
+REPO3=$(pos_get_variable repo_mpspdz --from-global)
+#REPO_COMMIT=$(pos_get_variable repo_commit --from-global)       
+REPO3_DIR=$(pos_get_variable repo_mpspdz_dir --from-global)
+
+# check WAN connection, waiting helps in most cases
+checkConnection() {
+    address=$1
+    i=0
+    maxtry=5
+    success=false
+    while [ $i -lt $maxtry ] && ! $success; do
+        success=true
+        echo "____ping $1 try $i" >> pinglog_external
+        ping -q -c 2 "$address" >> pinglog_external || success=false
+        ((++i))
+        sleep 2s
+    done
+    $success
+}
+
+checkConnection "mirror.lrz.de"
+echo 'unattended-upgrades unattended-upgrades/enable_auto_updates boolean false' | debconf-set-selections
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install -y automake build-essential git libboost-dev libboost-thread-dev parted \
+    libntl-dev libsodium-dev libssl-dev libtool m4 python3 texinfo yasm linux-cpupower \
+    python3-pip time software-properties-common iperf3
+# wget https://apt.llvm.org/llvm.sh
+# chmod +x llvm.sh
+# ./llvm.sh -y 15
+# apt install -y clang-15 gcc-12 g++-12
+
+
+echo 'deb http://deb.debian.org/debian testing main' > /etc/apt/sources.list.d/testing.list
+apt update -y
+apt install -y gcc-12 g++-12
+apt install libeigen3-dev
+# wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+# tar -xvf clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+# mv clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04 /usr/local/llvm-16
+# echo 'export PATH="/usr/local/llvm-16/bin:$PATH"' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH="/usr/local/llvm-16/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
+# source ~/.bashrc
+
+pip3 install -U numpy
+checkConnection "github.com"
+
+git clone "$REPO" "$REPO_DIR" # hpmcp
+git clone "$REPO2" "$REPO2_DIR" # mpcbench
+wget https://github.com/data61/MP-SPDZ/releases/download/v0.3.8/mp-spdz-0.3.8.tar.xz
+tar -xf mp-spdz-0.3.8.tar.xz 
+mv mp-spdz-0.3.8 "$REPO3_DIR" # MP-SPDZ
+
+
+################################# mpcbench ####################################
+cd "$REPO2_DIR" # mpcbench
+# git checkout fix
+cd ..
+
+# load custom htop config
+mkdir -p .config/htop
+cp "$REPO2_DIR"/helpers/htoprc ~/.config/htop/
+
+################################### hpmpc #####################################
+cd "$REPO_DIR" # hpmpc
+
+# use a stable state of the MP-Slice repo
+###git checkout "$REPO_COMMIT"
+
+# switch to fork
+# git checkout extended
+git checkout mp-spdz
+
+git clone https://github.com/chart21/flexNN.git SimpleNN
+cd SimpleNN
+git checkout hpmpc
+cd ..
+
+# adjust script to specific needs
+echo "wait" >> ./scripts/split-roles-3-execute.sh
+echo "wait" >> ./scripts/split-roles-3to4-execute.sh
+echo "wait" >> ./scripts/split-roles-4-execute.sh
+
+echo "global setup successful"
+ 
+################################## MP-SPDZ ####################################
+cd "$REPO3_DIR"
+
+cp "$REPO_DIR"/MP-SPDZ/Functions/bench/* ./Programs/Source/
---
+++ MP-SPDZ/bench_scripts/measurement.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+# shellcheck disable=SC1091,2154
+
+#
+# Script is run locally on experiment server.
+#
+
+# exit on error
+set -e
+# log every command
+set -x
+
+# load global variables
+REPO_DIR=$(pos_get_variable repo_hpmpc_dir --from-global) # hpmpc
+REPO2_DIR=$(pos_get_variable repo_dir --from-global) # mpcbench
+REPO3_DIR=$(pos_get_variable repo_mpspdz_dir --from-global) # mpspdz
+manipulate=$(pos_get_variable manipulate --from-global)
+# load loop variables/switches
+size=$(pos_get_variable input_size --from-loop)
+protocol=$(pos_get_variable protocol --from-loop)
+datatype=$(pos_get_variable datatype --from-loop)
+preprocess=$(pos_get_variable preprocess --from-loop)
+splitroles=$(pos_get_variable splitroles --from-loop)
+packbool=$(pos_get_variable packbool --from-loop)
+optshare=$(pos_get_variable optshare --from-loop)
+ssl=$(pos_get_variable ssl --from-loop)
+threads=$(pos_get_variable threads --from-loop)
+fun=$(pos_get_variable function --from-loop)
+txbuffer=$(pos_get_variable txbuffer --from-loop)
+rxbuffer=$(pos_get_variable rxbuffer --from-loop)
+verifybuffer=$(pos_get_variable verifybuffer --from-loop)
+comp="g++-12"
+
+timerf="%M (Maximum resident set size in kbytes)\n\
+%e (Elapsed wall clock time in seconds)\n\
+%P (Percent of CPU this job got)"
+player=$1
+environ=""
+# test types to simulate changing environments like cpu frequency or network latency
+read -r -a types <<< "$2"
+network=10.10."$3"
+partysize=$4
+# experiment type to allow small differences in experiments
+etype=$5
+touch testresults
+
+SCHEDULE_PATH="$REPO_DIR/MP-SPDZ/Schedules"
+BYTECODE_PATH="$REPO_DIR/MP-SPDZ/Bytecodes"
+FUNC_PATH="$REPO_DIR/MP-SPDZ/Functions/bench"
+
+cp "$FUNC_PATH"/* "$REPO3_DIR/Programs/Source"
+
+files=($(find "$FUNC_PATH" -type f -exec basename {} .mpc \;))
+
+mkdir -p "$SCHEDULE_PATH" "$BYTECODE_PATH"
+
+cd "$REPO3_DIR"
+
+for func in "${files[@]}"; do
+    echo "./compile.py $func $size"
+    ./compile.py -R 64 -X -K LTZ,EQZ "$func" "$size"
+    mv "$REPO3_DIR"/Programs/Schedules/"$func"-"$size".sch "$SCHEDULE_PATH"/"$func".sch
+done
+
+mv ./Programs/Bytecode/* "$BYTECODE_PATH"/
+
+cd "$REPO_DIR"
+
+# different split role script, different ip definition...
+if [ "$splitroles" -lt 1 ]; then
+    # define ip addresses of the other party members
+    if [ "$protocol" -lt 7 ]; then
+        [ "$player" -eq 0 ] && ipA="$network".3 && ipB="$network".4
+        [ "$player" -eq 1 ] && ipA="$network".2 && ipB="$network".4
+        [ "$player" -eq 2 ] && ipA="$network".2 && ipB="$network".3
+    else
+        [ "$player" -eq 0 ] && ipA="$network".3 && ipB="$network".4 && ipC="$network".5
+        [ "$player" -eq 1 ] && ipA="$network".2 && ipB="$network".4 && ipC="$network".5
+        [ "$player" -eq 2 ] && ipA="$network".2 && ipB="$network".3 && ipC="$network".5
+        [ "$player" -eq 3 ] && ipA="$network".2 && ipB="$network".3 && ipC="$network".4
+    fi
+else
+    # define all ips
+    ipA="$network".2
+    ipB="$network".3
+    ipC="$network".4
+    ipD="$network".5
+fi
+
+{
+    echo "./scripts/config.sh -p $player -n $size -d $datatype -s $protocol -e $preprocess -h $ssl -x $comp"
+
+    # set config and compile experiment
+    if [ "$splitroles" -eq 0 ]; then
+        /bin/time -f "$timerf" ./scripts/config.sh -p "$player" -n "$size" -d "$datatype" -x "$comp" \
+            -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 25000 \
+            -j "$threads" -f "$fun" -y "$txbuffer" -z "$rxbuffer" -m "$verifybuffer"
+    else
+        # with splitroles active, "-p 3" would through error. Omit -p as unneeded
+        /bin/time -f "$timerf" ./scripts/config.sh -n "$size" -d "$datatype" -x "$comp" \
+            -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 25000 \
+            -j "$threads" -f "$fun" -y "$txbuffer" -z "$rxbuffer" -m "$verifybuffer"
+    fi
+    
+    [ "$splitroles" -eq 1 ] && ./scripts/split-roles-3-compile.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -x "$comp"
+    [ "$splitroles" -eq 2 ] && ./scripts/split-roles-3to4-compile.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -d "$ipD" -x "$comp"
+    [ "$splitroles" -eq 3 ] && ./scripts/split-roles-4-compile.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -d "$ipD" -x "$comp"
+    
+    echo "$(du -BM run-P* | cut -d 'M' -f 1 | head -n 1) (Binary file size in MiB)"
+
+} |& tee testresults
+
+echo -e "\n========\n" >> testresults
+
+####
+#  environment manipulation section start
+####
+# shellcheck source=../host_scripts/manipulate.sh
+source "$REPO2_DIR"/host_scripts/hpmpc_mp-spdz/manipulate.sh
+
+if [[ "${types[*]}" == *" LATENCY=0 "* ]]; then
+    types=("${types[@]/LATENCY}")
+fi
+
+case " ${types[*]} " in
+    *" CPUS "*)
+        limitCPUs;;&
+    *" RAM "*)
+        limitRAM;;&
+    *" QUOTAS "*)
+        setQuota;;&
+    *" FREQS "*)
+        setFrequency;;&
+    *" BANDWIDTHS "*)
+        # check whether to manipulate a combination
+        case " ${types[*]} " in
+            *" LATENCIES "*)
+            case " ${types[*]} " in
+                *" PACKETDROPS "*)
+                    setAllParameters "$partysize";;
+                *)
+                setLatencyBandwidth;;
+            esac;;                 
+            *" PACKETDROPS "*) # a.k.a. packet loss
+                setBandwidthPacketdrop;;
+            *)
+                limitBandwidth;;
+        esac;;
+    *" LATENCIES "*)
+        if [[ " ${types[*]} " == *" PACKETDROPS "* ]]; then
+            setPacketdropLatency
+        else
+            setLatency
+        fi;;
+    *" PACKETDROPS "*)
+        setPacketdrop;;
+esac
+####
+#  environment manipulation section stop
+####
+
+success=true
+
+pos_sync --timeout 600
+
+# run the SMC protocol
+                              # skip 4th node here
+if [ "$splitroles" -eq 0 ]; then 
+    if [ "$protocol" -lt 7 ]; then
+        if [ "$player" -lt 3 ]; then
+            /bin/time -f "$timerf" timeout 1000s ./run-P"$player".o "$ipA" "$ipB" &>> testresults || success=false
+        fi
+    else
+        /bin/time -f "$timerf" timeout 1000s ./run-P"$player".o "$ipA" "$ipB" "$ipC" &>> testresults || success=false
+    fi
+elif [ "$splitroles" -eq 1 ] && [ "$player" -lt 3 ]; then
+    /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-3-execute.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" &>> testresults || success=false
+elif [ "$splitroles" -eq 2 ]; then
+    /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-3to4-execute.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -d "$ipD" &>> testresults || success=false
+elif [ "$splitroles" -eq 3 ]; then
+    /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-4-execute.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -d "$ipD" &>> testresults || success=false
+fi
+
+# divide external runtime x*j
+# Todo: divide normal binary run by j*j
+
+
+    # binary:   calculate mean of j results running concurrent ( /j *j )
+    # 3nodes:   calculate mean of 6*j results running concurrent ( /6*j *6*j )
+    # 3-4nodes: calculate mean of 24*j results running concurrent ( /24*j *24*j )
+    # 4nodes:   calculate mean of 24*j results running concurrent ( /24*j *24*j )
+#default divisor
+divisor=1
+divisorExt=1
+    
+    [ "$splitroles" -eq 0 ] && divisor=$((threads*threads)) && divisorExt=$((threads))
+    [ "$splitroles" -eq 1 ] && divisor=$((6*6*threads*threads)) && divisorExt=$((6*threads))
+    [ "$splitroles" -eq 2 ] && divisor=$((24*24*threads*threads)) && divisorExt=$((24*threads))
+    [ "$splitroles" -eq 3 ] && divisor=$((24*24*threads*threads)) && divisorExt=$((24*threads))
+
+    # sum=$(grep "measured to initialize program" testresults | cut -d 's' -f 2 | awk '{print $5}' | paste -s -d+ | bc)
+    # average=$(echo "scale=6;$sum / $divisor" | bc -l)
+    # echo "Time measured to initialize program: ${average}s" &>> testresults
+       max=$(grep "measured to initialize program" testresults | cut -d 's' -f 2 | awk '{print $5}' | sort -nr | head -1) 
+    average=$(echo "scale=6;$max / $divisorExt" | bc -l)
+    echo "Time measured to initialize program: ${average}s" &>> testresults
+
+    if [ "$preprocess" -eq 1 ]; then
+        # sum=$(grep "preprocessing chrono" testresults | cut -d 's' -f 4 | awk '{print $3}' | paste -s -d+ | bc)
+        # average=$(echo "scale=6;$sum / $divisor" | bc -l)
+    # echo "Time measured to perform preprocessing chrono: ${average}s" &>> testresults
+    max=$(grep "preprocessing chrono" testresults | cut -d 's' -f 4 | awk '{print $3}' | sort -nr | head -1) 
+        average=$(echo "scale=6;$max / $divisorExt" | bc -l)
+    echo "Time measured to perform preprocessing chrono: ${average}s" &>> testresults
+    fi
+
+    sum=$(grep "computation clock" testresults | cut -d 's' -f 2 | awk '{print $6}' | paste -s -d+ | bc)
+    average=$(echo "scale=6;$sum / $divisor" | bc -l)
+    echo "Time measured to perform computation clock: ${average}s" &>> testresults
+
+    sum=$(grep "computation getTime" testresults | cut -d 's' -f 2 | awk '{print $6}' | paste -s -d+ | bc)
+    average=$(echo "scale=6;$sum / $divisor" | bc -l)
+    echo "Time measured to perform computation getTime: ${average}s" &>> testresults
+
+max=$(grep "computation chrono" testresults | cut -d 's' -f 2 | awk '{print $6}' | sort -nr | head -1)
+    average=$(echo "scale=6;$max / $divisorExt" | bc -l)
+    echo "Time measured to perform computation chrono: ${average}s" &>> testresults
+# sum=$(grep "computation chrono" testresults | cut -d 's' -f 2 | awk '{print $6}' | paste -s -d+ | bc)
+    # average=$(echo "scale=6;$sum / $divisor" | bc -l)
+    # echo "Time measured to perform computation chrono: ${average}s" &>> testresults
+
+    runtimeext=$(grep "Elapsed wall clock" testresults | tail -n 1 | cut -d ' ' -f 1)
+    average=$(echo "scale=6;$runtimeext / $divisorExt" | bc -l)
+    echo "$average (Elapsed wall clock time in seconds)" &>> testresults
+
+
+pos_sync
+
+####
+#  environment manipulation reset section start
+####
+
+case " ${types[*]} " in
+
+    *" FREQS "*)
+        resetFrequency;;&
+    *" RAM "*)
+        unlimitRAM;;&
+    *" BANDWIDTHS "*|*" LATENCIES "*|*" PACKETDROPS "*)
+    	resetTrafficControl "$partysize";;&
+    *" CPUS "*)
+        unlimitCPUs
+esac
+
+####
+#  environment manipulation reset section stop
+####
+
+echo "experiment finished"  >> testresults
+pos_upload --loop testresults
+pos_upload --loop terminal_output.txt
+# abort if no success
+$success
