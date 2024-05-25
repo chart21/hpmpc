3b25bd87a040e285e4f69d18ec31caf3ff497560
chart21 <chart21@web.de>
2024-05-25 22:07:21 +0200

    bugfixes, updated dot prod

--- config.h
+++ config.h
@@ -39,10 +39,10 @@
 #define USE_CUDA_GEMM 0
 
 // How many gates should be buffered until sending them to the receiving party? 0 means the data of an entire communication round is buffered
-#define SEND_BUFFER 1000
+#define SEND_BUFFER 10000
 
 // How many reciving messages should be buffered until the main thread is signaled that data is ready? 0 means that all data of a communication round needs to be ready before the main thread is signaled.
-#define RECV_BUFFER 1000
+#define RECV_BUFFER 10000
 
 // How many messages should be buffered until a combined hash is performed? 0 means all hashes are calculated at the very end of the protocol.
 #define VERIFY_BUFFER 0
