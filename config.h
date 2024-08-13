e6e37a8dd9e13afab83d1001f295a1a838c4081b
chart21 <chart21@web.de>
2024-08-14 00:35:50 +0200

    imagenet

--- config.h
+++ config.h
@@ -101,11 +101,11 @@
 // === Neural Network Settings ===
 
 #ifndef MODELOWNER
-#define MODELOWNER P_0 // Who holds the model parameters? (-1: Dummy model parameters, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing). Important: Use "P_0" not "0"!
+#define MODELOWNER -1 // Who holds the model parameters? (-1: Dummy model parameters, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing). Important: Use "P_0" not "0"!
 #endif
 
 #ifndef DATAOWNER
-#define DATAOWNER P_1 // Who holds the data? (-1: Dummy dataset, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing). Important: Use "P_0" not "0"!
+#define DATAOWNER -1 // Who holds the data? (-1: Dummy dataset, P_0/P_1/P_2/P_3: Read locally from P_0/P_1/P_2/P_3 followed by secret sharing). Important: Use "P_0" not "0"!
 #endif
 
 #ifndef TRUNC_THEN_MULT
