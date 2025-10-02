8d9def29214b4291b0ad3665192fe2a32a92f314
Christopher Knop <chris.d.k.knop@gmail.com>
2025-10-02 19:23:39 +0200

    working Conv triples

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ set -e
 
 FUNC=182
 DATTYPE=32
-NUM_INPUTS=1
+NUM_INPUTS=10
 PROCESS_NUM=1
 
 STD="standard"
@@ -64,7 +64,7 @@ while [[ $# -gt 0 ]]; do
                 DATAOWNER=P_1 DATTYPE=${DATTYPE} PROTOCOL=4 \
                 NUM_INPUTS=${NUM_INPUTS} BITLENGTH=32 \
                 PROCESS_NUM=1 PRE=1 FAKE_TRIPLES=0 AB2_TRIPLES=1 BN2D_TRIPLES=0 \
-                FC_TRIPLES=1 CONV_TRIPLES=0
+                FC_TRIPLES=1 CONV_TRIPLES=1 INTERLEAVE_COMM=0
 
             ./scripts/run.sh -p all -n 2
             ;;
--- core/generate_beaver_tiples.hpp
+++ core/generate_beaver_tiples.hpp
@@ -550,10 +550,13 @@ void generateLayerDummyTriples(type** a,
                 std::cout << conv.n_filters << ", ";
                 std::cout << p.w_size_per_batch << "\n";
 
-                Iface::generateConvTriplesCheetahWrapper(uint_w[n], uint_x[n], uint_y + y_index_counter,
+                Iface::generateConvTriplesCheetahWrapper(
+                        PARTY == 1 ? uint_x[n] : nullptr, PARTY == 0 ? uint_w[n] : nullptr,
+                        uint_y + y_index_counter,
                         conv, p.batchSize,
                         ip, port, PARTY + 1, 1,
-                        AB2_TRIPLES == 0 ? Utils::PROTO::AB2 : Utils::PROTO::AB2);
+                        AB2_TRIPLES == 0 ? Utils::PROTO::AB2 : Utils::PROTO::AB2
+                );
             } else if constexpr (std::is_same_v<LayerParams, FullyConnectedParameter>) {
                 std::cout << params.size() << "FullyConnected\n";
                 std::cout << p.batchSize << ", ";
@@ -569,10 +572,6 @@ void generateLayerDummyTriples(type** a,
                         PARTY + 1, ip, port, 8,
                         Utils::PROTO::AB2
                 );
-
-                if (p.out_feat == 10)
-                    for (int i = 0; i < p.out_feat; ++i)
-                        std::cout << PARTY << " " << *(uint_y + y_index_counter + i) << "\n";
             } else if constexpr (std::is_same_v<LayerParams, BatchNorm2DParameter>) {
                 std::cout << params.size() << "BatchNorm\n";
             } else {
