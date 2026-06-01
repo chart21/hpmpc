9d5b2cad76c3cf72b41ffb470477f2a60b2978f5
Christopher Harth-Kitzerow <christopher.harth-kitzerow@bmw.de>
2026-06-01 21:03:01 +0200

    fix bug

--- programs/functions/share_conversion.hpp
+++ programs/functions/share_conversion.hpp
@@ -253,7 +253,7 @@ void bit_injection_opt_range(XOR_Share<Datatype, Share>* y, sint_t<Additive_Shar
         y[i].prepare_opt_bit_injection(val[i].get_share_pointer(), val[i].get_share_pointer());
 #if FUSE_RELU_AVG == 1 && (TRUNC_APPROACH == 4 || TRUNC_APPROACH == 0) && TRUNC_DELAYED == 0
       auto denominator = FloatFixedConverter<FLOATTYPE, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(curr_denom);
-      val[i].local_mult_and_trunc(PROMOTE(denominator));
+      val[i].local_mult_and_trunc(denominator);
 #endif
     }
     Share::communicate();
