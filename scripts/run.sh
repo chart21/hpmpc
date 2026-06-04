cec1e0d8b75b4517c8388d655fd4a2398c60d36d
Ajith Suresh <ajith.suresh@tii.ae>
2026-06-04 21:23:02 +0400

    Add -G player:device flag to run.sh/run_locally.sh for per-party GPU assignment on same machine

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,20 +2,32 @@
 
 helpFunction()
 {
-   echo "Script to run a n-PC protocol in a distributed setting"
-   echo -e "\t-p Party number"
-   echo -e "\t-a IP address of player 0 (if ip matches player_id can be empty)"
-   echo -e "\t-b IP address of player 1 (if ip matches player_id can be empty)"
-   echo -e "\t-c IP address of player 2 (if ip matches player_id can be empty)"
-   echo -e "\t-d IP address of player 3 (if ip matches player_id can be empty)"
-   echo -e "\t-n Number of players"
-   echo -e "\t-g Number of GPUs to use"
-   echo -e "\t-s SplitRoles Identifier"
-
-   exit 1 # Exit script after printing help
+   echo "Script to run a n-PC protocol in a distributed or local setting."
+   echo ""
+   echo "Usage: run.sh -p <party> [options]"
+   echo ""
+   echo "Options:"
+   echo -e "\t-p <party>       Party number, or 'all' to run all parties locally"
+   echo -e "\t-a <ip>          IP address of player 0"
+   echo -e "\t-b <ip>          IP address of player 1"
+   echo -e "\t-c <ip>          IP address of player 2"
+   echo -e "\t-d <ip>          IP address of player 3"
+   echo -e "\t-n <num>         Number of players"
+   echo -e "\t-g <num>         Number of GPUs to use (SplitRoles)"
+   echo -e "\t-s <id>          SplitRoles identifier"
+   echo -e "\t-G <player:dev>  Assign a specific CUDA device to a player (local runs only)."
+   echo -e "\t                 Can be repeated for multiple players."
+   echo -e "\t                 Players without -G get all GPUs (no restriction)."
+   echo -e "\t                 Examples:"
+   echo -e "\t                   -G 0:0 -G 1:1   → P0 uses GPU 0, P1 uses GPU 1"
+   echo -e "\t                   -G 1:2           → only P1 restricted to GPU 2"
+   echo -e "\t                   (no -G)          → all players see all GPUs"
+   exit 1
 }
 
-while getopts "p:a:b:c:d:n:g:s:" opt
+GPU_ARGS=()
+
+while getopts "p:a:b:c:d:n:g:s:G:h" opt
 do
    case "$opt" in
       p ) O_PARTY="$OPTARG" ;;
@@ -26,10 +38,11 @@ do
       n ) NUM_PLAYERS="$OPTARG" ;;
       g ) NUM_GPUS="$OPTARG" ;;
       s ) SPLIT_ROLES="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+      G ) GPU_ARGS+=("-G" "$OPTARG") ;;
+      h ) helpFunction ;;
+      ? ) helpFunction ;;
    esac
 done
-
 
 O_IP1="127.0.0.1"
 O_IP2="127.0.0.1"
@@ -39,46 +52,18 @@ O_NUM_PLAYERS=3
 O_NUM_GPUS=0
 O_SPLIT_ROLES=0
 
-if [ ! -z "$IP1" ];
-then
-O_IP1="$IP1"
-fi
+[ -n "$IP1" ]        && O_IP1="$IP1"
+[ -n "$IP2" ]        && O_IP2="$IP2"
+[ -n "$IP3" ]        && O_IP3="$IP3"
+[ -n "$IP4" ]        && O_IP4="$IP4"
+[ -n "$NUM_PLAYERS" ] && O_NUM_PLAYERS="$NUM_PLAYERS"
+[ -n "$NUM_GPUS" ]   && O_NUM_GPUS="$NUM_GPUS"
+[ -n "$SPLIT_ROLES" ] && O_SPLIT_ROLES="$SPLIT_ROLES"
 
-if [ ! -z "$IP2" ];
-then
-O_IP2="$IP2"
-fi
-
-if [ ! -z "$IP3" ];
-then
-O_IP3="$IP3"
-fi
-
-if [ ! -z "$IP4" ];
-then
-O_IP4="$IP4"
-fi
-
-if [ ! -z "$NUM_PLAYERS" ];
-then
-O_NUM_PLAYERS="$NUM_PLAYERS"
-fi
-
-if [ ! -z "$NUM_GPUS" ];
-then
-O_NUM_GPUS="$NUM_GPUS"
-fi
-
-if [ ! -z "$SPLIT_ROLES" ];
-then
-O_SPLIT_ROLES="$SPLIT_ROLES"
-fi
-
-#if O_SPLIT_ROLES is 0, then run the protocol with 3 players
 if [ "$O_SPLIT_ROLES" = "0" ]; then
     if [ "$O_PARTY" = "all" ]
     then
-        scripts/run_locally.sh -n $O_NUM_PLAYERS
+        scripts/run_locally.sh -n $O_NUM_PLAYERS "${GPU_ARGS[@]}"
     else
         scripts/run_distributed.sh -p $O_PARTY -a $O_IP1 -b $O_IP2 -c $O_IP3 -d $O_IP4
     fi
@@ -89,4 +74,3 @@ elif [ "$O_SPLIT_ROLES" = "2" ]; then
 elif [ "$O_SPLIT_ROLES" = "3" ]; then
     scripts/split-roles-4-execute.sh -p $O_PARTY -a $O_IP1 -b $O_IP2 -c $O_IP3 -d $O_IP4
 fi
-
--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -2,24 +2,44 @@
 helpFunction()
 {
    echo "Script to run n players locally after compiling."
-   echo -e "\t-f Executable to run"
-   echo -e "\t-n num_players"
-   exit 1 # Exit script after printing help
+   echo ""
+   echo "Usage: run_locally.sh [-f executable] -n num_players [-G player:device] ..."
+   echo ""
+   echo "Options:"
+   echo -e "\t-f <name>        Executable prefix to run (default: run)"
+   echo -e "\t-n <num>         Number of players"
+   echo -e "\t-G <player:dev>  Assign a specific CUDA device to a player."
+   echo -e "\t                 Can be repeated for multiple players."
+   echo -e "\t                 Players without -G get all GPUs (no restriction)."
+   echo -e "\t                 Examples:"
+   echo -e "\t                   -G 0:0 -G 1:1   → P0 uses GPU 0, P1 uses GPU 1"
+   echo -e "\t                   -G 1:2           → only P1 restricted to GPU 2"
+   echo -e "\t                   (no -G)          → all players see all GPUs"
+   exit 1
 }
 
-while getopts "f:n:" opt
+FUNCTION="run"
+NUM_PLAYERS=""
+declare -A GPU_MAP   # GPU_MAP[player]=device
+
+while getopts "f:n:G:h" opt
 do
    case "$opt" in
       f ) FUNCTION="$OPTARG" ;;
       n ) NUM_PLAYERS="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+      G )
+         PLAYER="${OPTARG%%:*}"
+         DEVICE="${OPTARG##*:}"
+         if [ "$PLAYER" = "$OPTARG" ] || [ -z "$PLAYER" ] || [ -z "$DEVICE" ]; then
+             echo "❌ -G requires format player:device (e.g. -G 0:0)"
+             exit 1
+         fi
+         GPU_MAP[$PLAYER]="$DEVICE"
+         ;;
+      h ) helpFunction ;;
+      ? ) helpFunction ;;
    esac
 done
-
-if [ -z "$FUNCTION" ]
-then
-    FUNCTION="run"
-fi
 
 if [ -z "$NUM_PLAYERS" ]
 then
@@ -29,7 +49,12 @@ fi
 
 for (( i=0; i<$NUM_PLAYERS; i++ ))
 do
-    ./executables/"$FUNCTION"-P"$i".o &
+    if [ -n "${GPU_MAP[$i]+set}" ]; then
+        echo "🎮 P$i → CUDA_VISIBLE_DEVICES=${GPU_MAP[$i]}"
+        CUDA_VISIBLE_DEVICES="${GPU_MAP[$i]}" ./executables/"$FUNCTION"-P"$i".o &
+    else
+        ./executables/"$FUNCTION"-P"$i".o &
+    fi
 done
 
 wait
